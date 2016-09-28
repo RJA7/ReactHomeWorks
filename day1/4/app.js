3f7342f06eecea979e59d9785351170e2ba50e06
RJA <kopanskyy.roman@gmail.com>
2016-09-28 17:24:54 +0300

    - data

--- day1/4/app.js
+++ day1/4/app.js
@@ -16,13 +16,13 @@ const Member = class extends Component {
                     <div className="member-city-block">
                         <img src="img/city.png" />
                         <div className="member-city">
-                            {member.city}
+                            {member.location}
                         </div>
                     </div>
                     <div className="member-socials">
                         <a href={member.facebook}><img src="img/facebook.png" /></a>
-                        <a href={member.tweeter}><img src="img/tweeter.png" /></a>
-                        <a href={'mailto:' + member.facebook}><img src="img/email.png" /></a>
+                        <a href={member.github}><img src="img/tweeter.png" /></a>
+                        <a href={'mailto:' + member.email}><img src="img/email.png" /></a>
                     </div>
                 </div>
             </div>
@@ -55,12 +55,283 @@ const App = class extends Component {
 
 const members = [
     {
-        name    : 'Roma',
-        city    : 'Lviv',
-        avatar  : 'https://avatars2.githubusercontent.com/u/16480809?v=3&s=466',
-        facebook: 'facebook',
-        tweeter : 'tweeter',
-        email   : 'a@i.ua'
+        name    : 'Petya Krasnopir',
+        avatar  : 'https://github.com/mrbob89.png',
+        location: 'Борисполь',
+        email   : 'p.krasnopir@smartweb.com.ua',
+        github  : 'https://github.com/mrbob89',
+        facebook: 'https://www.facebook.com/undersun.under'
+
+    },
+    {
+        name    : 'Kirill Litvinenko',
+        avatar  : 'https://github.com/KirillLitvinenko.png',
+        location: 'Киев',
+        email   : 'litvinenkokirill@gmail.com',
+        github  : 'https://github.com/KirillLitvinenko',
+        facebook: 'https://www.facebook.com/kirill.litvinenko.39'
+
+    },
+    {
+        name    : 'Sergey Rubets',
+        avatar  : 'https://github.com/sergeyamator.png',
+        location: 'Kiev',
+        email   : 'sramator@gmail.com',
+        github  : 'https://github.com/sergeyamator',
+        facebook: 'https://www.facebook.com/profile.php?id=100006979031096'
+
+    },
+    {
+        name    : 'Andrii Bykovets',
+        avatar  : 'https://github.com/ivarden.png',
+        location: 'Kiev',
+        email   : 'Vizantiyec@gmail.com',
+        github  : 'https://github.com/ivarden',
+        facebook: 'https://www.facebook.com/Vizantiyec'
+
+    },
+    {
+        name    : 'Yerbol Kuandykov',
+        avatar  : 'https://github.com/vslepkan.png',
+        location: 'Москва',
+        email   : 'ereke@list.ru',
+        github  : 'https://github.com/vslepkan',
+        facebook: 'https://facebook.com/yerbol.kuandykov'
+
+    },
+    {
+        name    : 'Anton Honcharov',
+        avatar  : 'https://github.com/Erreke.png',
+        location: 'Kyiv',
+        email   : '4eyalush@gmail.com',
+        github  : 'https://github.com/Erreke',
+        facebook: 'https://www.facebook.com/4eyal'
+
+    },
+    {
+        name    : 'Andrey Paniuta',
+        avatar  : 'https://github.com/mercury99.png',
+        location: 'Kyiv',
+        email   : 'andreypanuta@gmail.com',
+        github  : 'https://github.com/mercury99',
+        facebook: 'https://www.facebook.com/a.paniuta'
+
+    },
+    {
+        name    : 'Ефим Пасианиди',
+        avatar  : 'https://github.com/apaniuta.png',
+        location: 'Москва',
+        email   : 'efimweb@gmail.com',
+        github  : 'https://github.com/apaniuta',
+        facebook: 'https://www.facebook.com/efimweb'
+
+    },
+    {
+        name    : 'Kolodyazhniy Vladimir',
+        avatar  : 'https://github.com/efimweb.png',
+        location: 'Харьков',
+        email   : 'vkolodyazhniy@acceptic.net',
+        github  : 'https://github.com/efimweb',
+        facebook: 'https://www.facebook.com/vladimir.kolodtazhny'
+
+    },
+    {
+        name    : 'Andrii Korol',
+        avatar  : 'https://github.com/vladimirkolodyazhniy.png',
+        location: 'Киев',
+        email   : 'akorol777@ukr.net',
+        github  : 'https://github.com/vladimirkolodyazhniy',
+        facebook: 'https://www.facebook.com/andrey.king.16'
+
+    },
+    {
+        name    : 'Ivan Lapa',
+        avatar  : 'https://github.com/akorol777.png',
+        location: 'Киев',
+        email   : 'sstage44@gmail.com',
+        github  : 'https://github.com/akorol777',
+        facebook: 'https://www.facebook.com/john.lapa.1'
+
+    },
+    {
+        name    : 'Roman Boboshko',
+        avatar  : 'https://github.com/eyale.png',
+        location: 'Kiev',
+        email   : 'romanboboshko89@gmail.com',
+        github  : 'https://github.com/eyale',
+        facebook: 'https://www.facebook.com/roma.boboshko'
+
+    },
+    {
+        name    : 'Varden Adamchuk',
+        avatar  : 'https://github.com/burnjohn.png',
+        location: 'Burnaby, BC, Canada',
+        email   : 'varden.adams@gmail.com',
+        github  : 'https://github.com/burnjohn',
+        facebook: 'https://facebook.com/varden.adamchuk'
+
+    },
+    {
+        name    : 'Gavrilov Evgen',
+        avatar  : 'https://github.com/mygeweb.png',
+        location: 'Odessa',
+        email   : 'gavrilov.web@gmail.com',
+        github  : 'https://github.com/mygeweb',
+        facebook: 'https://www.facebook.com/profile.php?id=100011282259899'
+
+    },
+    {
+        name    : 'Sergey Kravchenko',
+        avatar  : 'https://github.com/coder4u.png',
+        location: 'Николаев',
+        email   : 'coder4u@mail.ru',
+        github  : 'https://github.com/coder4u',
+        facebook: 'https://www.facebook.com/skravcenko'
+
+    },
+    {
+        name    : 'Andrew Weber',
+        avatar  : 'https://github.com/AndreyWeber.png',
+        location: 'Wroclaw',
+        email   : 'Andrey.Weber@gmail.com',
+        github  : 'https://github.com/AndreyWeber',
+        facebook: 'https://www.facebook.com/andreyweber'
+
+    },
+    {
+        name    : 'Taras Zakandaev',
+        avatar  : 'https://github.com/taras-zak.png',
+        location: 'Kiev',
+        email   : 'taras.zakan@gmail.com',
+        github  : 'https://github.com/taras-zak',
+        facebook: 'https://www.facebook.com/taras.zak'
+
+    },
+    {
+        name    : 'Антон Смирно',
+        avatar  : 'https://github.com/smirAnton.png',
+        location: 'Киев',
+        email   : 'smiranton.kiev@gmail.com',
+        github  : 'https://github.com/smirAnton',
+        facebook: 'https://www.facebook.com/anton.smirnov.5621'
+
+    },
+    {
+        name    : 'Maksym Borduzha',
+        avatar  : 'https://github.com/MaStep92.png',
+        location: 'Киев',
+        email   : 'paralyzedd@gmail.com',
+        github  : 'https://github.com/MaStep92',
+        facebook: 'https://www.facebook.com/maksym.borduzha'
+
+    },
+    {
+        name    : 'Iryna Prystupa',
+        avatar  : 'https://github.com/IraPristupa.png',
+        location: 'Киев',
+        email   : 'irochkapristupa@gmail.com',
+        github  : 'https://github.com/IraPristupa',
+        facebook: 'https://www.facebook.com/profile.php?id=100009826605846'
+
+    },
+    {
+        name    : 'Dima Mostovyi',
+        avatar  : 'https://github.com/Vizantiyec.png',
+        location: 'Киев',
+        email   : 'djmakaron2009@gmail.com',
+        github  : 'https://github.com/Vizantiyec',
+        facebook: 'https://www.facebook.com/dima.mostovoy'
+
+    },
+    {
+        name    : 'Eldar Gosin',
+        avatar  : 'https://github.com/distolma.png',
+        location: 'Rishon',
+        email   : 'eldargosin@gmail.com',
+        github  : 'https://github.com/distolma',
+        facebook: 'https://www.facebook.com/eldar.gosin'
+
+    },
+    {
+        name    : 'Anton Vavilov',
+        avatar  : 'https://github.com/eldargosin.png',
+        location: 'Вологда',
+        email   : 'vavilovonline@gmail.com',
+        github  : 'https://github.com/eldargosin',
+        facebook: 'https://www.facebook.com/vavilovonline'
+
+    },
+    {
+        name    : 'Ivan Kudinov',
+        avatar  : 'https://github.com/Krasnopir.png',
+        location: 'Липецк',
+        email   : 'atom_lipetsk@list.ru',
+        github  : 'https://github.com/Krasnopir',
+        facebook: 'https://www.facebook.com/profile.php?id=100011499751006'
+
+    },
+    {
+        name    : 'Andrey Ponomarenko',
+        avatar  : 'https://github.com/smi2.png',
+        location: 'Kiev',
+        email   : 'andrey212@gmail.com',
+        github  : 'https://github.com/smi2',
+        facebook: 'https://www.facebook.com/andrey.ponomarenko.165'
+
+    },
+    {
+        name    : 'Albert Kumyshev',
+        avatar  : 'https://github.com/ooaeoaey.png',
+        location: 'Москва',
+        email   : 'phemistokl@gmail.com',
+        github  : 'https://github.com/ooaeoaey',
+        facebook: 'https://www.facebook.com/albert.kumyshev'
+
+    },
+    {
+        name    : 'Mihail Lardugin',
+        avatar  : 'https://github.com/phemistokl.png',
+        location: 'Москва',
+        email   : 'mihail.lardugin@gmail.com',
+        github  : 'https://github.com/phemistokl',
+        facebook: 'https://m.facebook.com/mihail.lardugin'
+
+    },
+    {
+        name    : 'Nataliya Kokoreva',
+        avatar  : 'https://github.com/lardugin.png',
+        location: 'Москва',
+        email   : 'athalie@yandex.ru',
+        github  : 'https://github.com/lardugin',
+        facebook: ""
+
+    },
+    {
+        name    : 'Vladyslav Slepkan',
+        avatar  : 'https://github.com/Athalie.png',
+        location: 'Kiev',
+        email   : 'vslepkan@gmail.com',
+        github  : 'https://github.com/Athalie',
+        facebook: 'https://www.facebook.com/vslepkan'
+
+    },
+    {
+        name    : 'Alexander Kostushin',
+        avatar  : 'https://github.com/alukos.png',
+        location: 'Санкт Петербург',
+        email   : 'alukos@yandex.ru',
+        github  : 'https://github.com/alukos',
+        facebook: ""
+
+    },
+    {
+        name    : 'Olkhovyk Olena',
+        avatar  : 'https://github.com/Elena363.png',
+        location: 'Kiev',
+        email   : 'elena-2820@yandex.ua',
+        github  : 'https://github.com/Elena363',
+        facebook: 'https://www.facebook.com/elen.olkhovyk'
+
     }
 ];
 
